6c620f72906e1b389907892bf380ec1fdfe009d3
Titan antony <antonytitan96@gmail.com>
2025-10-18 15:22:52 +0300

    Chore: Type conversion and math operations

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # === Hello World ====
 
-print('Antony Titan')
+'''print('Antony Titan')
 
 print('0----')
 print(' IIII')
 print('*' * 10)
-
+'''
 ''' PYTHON VARIABLES '''
-price = 10
+'''price = 10
 price = 20
 rating = 4.5
 _isPublished = True
@@ -15,17 +15,27 @@ project = 'Python Recap'
 
 print('price', price)
 print('rating', rating)
-
+'''
 
 ''' Patient Exercise'''
 '''name = 'John Smith'
 age = 20
 _isNewPatient = True'''
 
-name = input('What is your name? ')
+'''name = input('What is your name? ')
 color = input('What is your favourite colour? ')
 print(name + '\'s favourite colour is ' + color)
 
+'''
 
+'''birth_year = input('What is your year of birth? ')
 
+age = 2025 - int(birth_year)
 
+print(age)'''
+
+weight_kg = input('What is your weight in kg\'s? ')
+
+weight_pounds = 2.204623 * float(weight_kg)
+
+print('Your weight is', weight_pounds, 'pounds' )
