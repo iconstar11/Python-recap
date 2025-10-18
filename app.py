f930b05d6f074fd702e5abdd97a73e6ea63d2c8a
Titan antony <antonytitan96@gmail.com>
2025-10-18 13:57:08 +0300

    Chore: Code for basic sytax variable and input funtion

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
+# === Hello World ====
+
 print('Antony Titan')
+
+print('0----')
+print(' IIII')
+print('*' * 10)
+
+''' PYTHON VARIABLES '''
+price = 10
+price = 20
+rating = 4.5
+_isPublished = True
+project = 'Python Recap'
+
+print('price', price)
+print('rating', rating)
+
+
+''' Patient Exercise'''
+'''name = 'John Smith'
+age = 20
+_isNewPatient = True'''
+
+name = input('What is your name? ')
+color = input('What is your favourite colour? ')
+print(name + '\'s favourite colour is ' + color)
+
+
+
+
