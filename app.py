ffb58624ddfd8bb558e7a21ee9c2eedc8fbc207b
Titan antony <antonytitan96@gmail.com>
2025-10-23 15:41:04 +0300

    Chore: conditional statement examples: if statements

--- app.py
+++ app.py
@@ -1,3 +1,56 @@
+weight = input('Weight: ')
+unit_of_Measure = input('L(bs) or K(g): ')
+
+if unit_of_Measure.upper() == 'K':
+    L = int(weight) * 2.2
+    print(f'You are {L} pounds')
+elif unit_of_Measure.upper() == 'L':
+    Kg = int(weight) * 0.45
+    print(f'You are {Kg} Kgs')
+else: print('Enter a valid unit of measure or weight')
+
+'''name = input('Whats your name? ')
+
+
+if len(name) < 3:
+    print('Name must have more than 2 characters')
+elif len(name) > 50:
+    print('Name cannot have more than 50 characters')
+else: print(f'Hi {name}')
+'''
+'''temp =9
+
+if temp>30:
+    print('Its a hot day')
+elif temp<10:
+    print('Its a cold day')
+else: print('Its neither hot or cold')'''
+
+'''good_credit_worth = False
+
+price = 1000000
+good_dic = int(price * 90 /100)
+bad_cred_dic = int(price * 80 /100)
+
+
+if good_credit_worth:
+    print(f'You need to put down a payment of ${good_dic} as down payment')
+else:
+    print(f'You need to put down a payment of ${bad_cred_dic} as down payment')
+
+'''
+'''is_hot = False
+is_cold = False
+
+if is_hot:
+    print('Its a hot day')
+elif is_cold:
+    print('Its a cold day')
+else:
+    print('Its a lovely day')
+print('Enjoy your day')'''
+
+
 import math
 
 # === Arithmetic Operations ====
@@ -22,11 +75,11 @@ print(x + y)
 
 #======Math Functions=====
 
-z = 2.4
+'''z = 2.4
 
 print(abs(z))
 print(math.ceil(z))
-
+'''
 
 
 
